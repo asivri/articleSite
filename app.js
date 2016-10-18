1a1f4c68fb74bb20d9b2b5ffef8e98dce6f96375
Ahmet <asivri@fredonia.edu>
2016-10-18 03:20:03 +0300

    Done with basic configurations

--- app.js
+++ app.js
@@ -7,14 +7,16 @@ var bodyParser = require('body-parser');
 //Following step is creating variables for the dependencies:
 var mongoDB = require('mongodb');
 var varMongoose = require('mongoose');
-var dbVar = mongoose.connection;
+var dbVar = varMongoose.connection;
 var connectFlash = require('connect-flash');
 var expressMessages = require('express-messages');
-var expressValidator = require('express-validator')
+var expressValidator = require('express-validator');
 var expressSession = require('express-session');
 var varPassport = require('passport');
 var passportLocal = require('passport-local').Strategy; //Special for PassportJS
+//Multer setup (For the file upload processes)
 var uploadImage = require('multer');
+var upload = uploadImage({dest: './images'});
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -25,6 +27,8 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -32,6 +36,43 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+//Express-session setup
+app.use(expressSession({
+  secret: 'asivri', //The secret used to sing the session ID cookie. Can be anything
+  saveUninitialized: true, //Forces a session that is "uninitialized" to be saved to the store.
+  //The doc of express-session says it's handy to set it as false for storage usage etc.
+  resave: true
+}));
+
+//Passport setup
+app.use(varPassport.initialize());
+app.use(varPassport.session());
+
+//Validator
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+    var namespace = param.split('.')
+        , root    = namespace.shift()
+        , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+//Express-messages setup
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
 
 app.use('/', routes);
 app.use('/users', users);
